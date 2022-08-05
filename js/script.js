60d449be88345408690d217b4e72f3284623dcb8
tauri_s <tauristclaire@gmail.com>
2022-08-04 18:26:10 -0700

    JS to remove duplicates and produce numbered list without them

--- js/script.js
+++ js/script.js
@@ -18,3 +18,25 @@ var groceries = [
   "olive oil",
   "mustard"
 ];
+
+
+//Create function called list, an array space for all nonduplicates to load
+//For of loop where each element is named item, checks each item and conditional cleanList DOES NOT include already it is added
+//Return the cleanList
+var deleteDuplicates = function (list) {
+  var cleanList = [];
+  for (var item of list) {
+    if (!cleanList.includes(item)) {
+      cleanList.push(item);
+    }
+  }
+    return cleanList;
+  };
+
+//Call to deleteDuplicates function with groceries as arguement
+var newGroceries = deleteDuplicates(groceries);
+
+//Log out index of groceries minus the items that were pulled from last function
+groceries.forEach(function (item, index) {
+  console.log(`#${index + 1} - ${item}`)
+});
